096489b11fbb069a9925fca67b77b102604c6346
TylerHamiltonJ <tylerhamilton@Tylers-iMac.local>
2016-12-15 15:57:48 +1100

    Initialising

---
+++ wp-content/themes/custom/template-parts/header/site-branding.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Displays header site branding
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-branding">
+	<div class="wrap">
+<div class="header-left headerElements">
+		<?php the_custom_logo(); ?>
+</div>
+<div class="header-middle headerElements">     
+		<div class="site-branding-text">
+			<?php if ( is_front_page() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php endif; ?>
+
+			<?php $description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+					<p class="site-description"><?php echo $description; ?></p>
+				<?php endif; ?>
+		</div>
+</div>   
+<div class="header-right headerElements">
+<?php get_search_form(); ?>        
+</div>        
+        
+        <!-- .site-branding-text -->
+
+		
+
+	</div><!-- .wrap -->
+</div><!-- .site-branding -->
